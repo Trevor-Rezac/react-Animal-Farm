4949b80ed5987bc4713b03a03b799d06b852e454
TrevorRezac <trev.rezac@gmail.com>
2022-01-25 21:42:26 -0800

    added email to footer

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <Header name='Trevor Rezac' className="App-header"></Header>
       <Main animals={ animals }></Main>
-      <Footer name='Trevor'></Footer>
+      <Footer name='Trevor' email='trev.rezac@gmail.com'></Footer>
     </div>
   );
 }
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,5 +2,5 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer(props) {
-  return <footer>Coded by {props.name}</footer>;
+  return <footer>Coded by {props.name} - Contact Me! {props.email}</footer>;
 }
